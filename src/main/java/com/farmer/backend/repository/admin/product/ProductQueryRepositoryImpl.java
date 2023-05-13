ffb4cf8eb2f0f62195c3aedbba56fab06898cb75
wolveshowl <dipper25@naver.com>
2023-05-13 19:07:53 +0900

    add queryRepository

--- src/main/java/com/farmer/backend/repository/admin/product/ProductQueryRepositoryImpl.java
+++ src/main/java/com/farmer/backend/repository/admin/product/ProductQueryRepositoryImpl.java
@@ -1,4 +1,7 @@
 package com.farmer.backend.repository.admin.product;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProductQueryRepositoryImpl implements ProductQueryRepository {
 }
