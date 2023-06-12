408500b17fefc79428c1947a77f1c544c045d15e
jnk <dipper25@naver.com>
2023-06-13 08:41:17 +0900

    swagger 배포 및 API 호출 인가 해제

--- src/main/java/com/farmer/backend/config/SwaggerConfig.java
+++ src/main/java/com/farmer/backend/config/SwaggerConfig.java
@@ -1,21 +1,23 @@
 package com.farmer.backend.config;
 
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
+    @Bean
+    public GroupedOpenApi jwtApi() {
+        return GroupedOpenApi.builder()
+                .group("jwt-api")
+                .pathsToMatch("/**")
+                .build();
+    }
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
