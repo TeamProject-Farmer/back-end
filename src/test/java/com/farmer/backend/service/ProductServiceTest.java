748112a6f2fb14ef114a7feea13039e360a05e61
jnk <dipper25@naver.com>
2023-07-03 04:29:39 +0900

    feat: 상품 엔티티 테스트 코드 작성(전체상품, MDPICK)

--- src/test/java/com/farmer/backend/service/ProductServiceTest.java
+++ src/test/java/com/farmer/backend/service/ProductServiceTest.java
@@ -1,7 +1,11 @@
 package com.farmer.backend.service;
 
+import com.farmer.backend.api.controller.product.response.ResponseProductDtoList;
 import com.farmer.backend.api.service.product.ProductService;
+import com.farmer.backend.domain.product.ProductOrderCondition;
+import com.farmer.backend.domain.product.ProductQueryRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
@@ -18,14 +25,24 @@ import java.util.Map;
 class ProductServiceTest {
 
     @Autowired
-    ProductService productService;
+    ProductQueryRepository productQueryRepositoryImpl;
 
     @Test
-    @DisplayName("상품 등록")
-    void createProduct() {
-        Map<Long, String> categoryList = productService.getCategoryList();
+    @DisplayName("상품 전체 리스트")
+    void productList() {
+        List<ResponseProductDtoList> productList = productQueryRepositoryImpl.productList(ProductOrderCondition.NEWS);
+        for (ResponseProductDtoList product : productList) {
+            log.info("product={}", product.getProductName());
+        }
+    }
 
-
+    @Test
+    @DisplayName("MD PICK 상품 리스트")
+    void mdPickList() {
+        List<ResponseProductDtoList> mdPickList = productQueryRepositoryImpl.mdPickList();
+        for (ResponseProductDtoList product : mdPickList) {
+            log.info("product={}", product.getProductName());
+        }
     }
 
 }
