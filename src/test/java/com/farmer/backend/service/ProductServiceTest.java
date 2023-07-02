ca64dce0a0b8fda0137341e0e65b9208d01f80c2
jnk <dipper25@naver.com>
2023-07-03 05:49:44 +0900

    feat: 상품 상세페이지 테스트 코드 작성

--- src/test/java/com/farmer/backend/service/ProductServiceTest.java
+++ src/test/java/com/farmer/backend/service/ProductServiceTest.java
@@ -1,13 +1,19 @@
 package com.farmer.backend.service;
 
+import com.farmer.backend.api.controller.options.response.ResponseOptionDto;
+import com.farmer.backend.api.controller.product.response.ResponseProductDto;
 import com.farmer.backend.api.controller.product.response.ResponseProductDtoList;
 import com.farmer.backend.api.controller.product.response.ResponseShopBySizeProduct;
 import com.farmer.backend.api.controller.productcategory.response.ResponseCategoryDto;
 import com.farmer.backend.api.service.product.ProductService;
+import com.farmer.backend.domain.options.OptionRepository;
 import com.farmer.backend.domain.product.ProductOrderCondition;
 import com.farmer.backend.domain.product.ProductQueryRepository;
+import com.farmer.backend.domain.product.ProductRepository;
 import com.farmer.backend.domain.product.ProductSize;
 import com.farmer.backend.domain.product.productcategory.ProductCategoryRepository;
+import com.farmer.backend.exception.CustomException;
+import com.farmer.backend.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -30,9 +36,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ProductServiceTest {
 
     @Autowired
+    ProductRepository productRepository;
+    @Autowired
     ProductQueryRepository productQueryRepositoryImpl;
     @Autowired
     ProductCategoryRepository productCategoryRepository;
+    @Autowired
+    OptionRepository optionRepository;
 
     @Test
     @DisplayName("상품 전체 리스트")
@@ -75,6 +85,15 @@ class ProductServiceTest {
     void shopBySizeProductOne() {
         ResponseShopBySizeProduct shopBySizeProductOne = productQueryRepositoryImpl.findByShopBySizeProductOne(ProductSize.M);
         log.info("productOne={}", shopBySizeProductOne.getProductCategoryId());
+    }
+
+    @Test
+    @DisplayName("상품 상세 조회")
+    void productDetail() {
+        Long productId = 5L;
+        List<ResponseOptionDto> options = optionRepository.findByProductId(productId).stream().map(ResponseOptionDto::optionList).collect(Collectors.toList());
+        ResponseProductDto productDto = productRepository.findById(productId).map(product -> ResponseProductDto.getOneProduct(product, options)).orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+        log.info("productDto={}", productDto.getName());
     }
 
 }
