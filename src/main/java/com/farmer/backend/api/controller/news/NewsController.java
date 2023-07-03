c785f97d94114e2a26cc2cd3e7bee8f32bb4f308
jnk <dipper25@naver.com>
2023-07-04 03:53:04 +0900

    feat: 기사 도메인 API Docs 기능 추가

--- src/main/java/com/farmer/backend/api/controller/news/NewsController.java
+++ src/main/java/com/farmer/backend/api/controller/news/NewsController.java
@@ -2,6 +2,8 @@ package com.farmer.backend.api.controller.news;
 
 import com.farmer.backend.api.controller.news.response.ResponseNewsDto;
 import com.farmer.backend.api.service.news.NewsService;
+import com.farmer.backend.config.ApiDocumentResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +22,12 @@ public class NewsController {
 
     private final NewsService newsService;
 
+    /**
+     * 메인 페이지 기사 정보
+     * @return
+     */
+    @ApiDocumentResponse
+    @Operation(summary = "뉴스 정보 조회", description = "메인 페이지 뉴스 정보를 조회합니다.")
     @GetMapping
     public ResponseNewsDto newsInfo() {
         return newsService.findNewsInfo();
