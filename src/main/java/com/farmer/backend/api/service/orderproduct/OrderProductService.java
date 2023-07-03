0c50e91ce9d777bea1158ef87e31c5377f7ec4e0
jnk <dipper25@naver.com>
2023-07-04 03:46:52 +0900

    feat: 마이페이지 구매목록 기능 개발

---
+++ src/main/java/com/farmer/backend/api/service/orderproduct/OrderProductService.java
@@ -0,0 +1,54 @@
+package com.farmer.backend.api.service.orderproduct;
+
+import com.farmer.backend.api.controller.orderproduct.response.ResponseOrderProductDto;
+import com.farmer.backend.api.controller.product.response.ResponseProductDto;
+import com.farmer.backend.domain.member.Member;
+import com.farmer.backend.domain.member.MemberRepository;
+import com.farmer.backend.domain.orderproduct.OrderProduct;
+import com.farmer.backend.domain.orderproduct.OrderProductQueryRepository;
+import com.farmer.backend.domain.orderproduct.OrderProductRepository;
+import com.farmer.backend.domain.orders.OrderRepository;
+import com.farmer.backend.domain.orders.Orders;
+import com.farmer.backend.exception.CustomException;
+import com.farmer.backend.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderProductService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final OrderProductRepository orderProductRepository;
+    private final OrderProductQueryRepository orderProductQueryRepositoryImpl;
+
+    /**
+     * 마이페이지 -> 구매목록 기능(최대 6까지 데이터 출력)
+     * @param username 회원 이메일
+     * @return ResponseOrderProductDto
+     */
+    @Transactional(readOnly = true)
+    public List<ResponseOrderProductDto> shoppingList(String username) {
+        Member findMember = memberRepository.findByEmail(username).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        List<Orders> orderList = orderRepository.findByMemberId(findMember.getId());
+        List<ResponseOrderProductDto> responseOrderProductDto = new ArrayList<>();
+
+        for (Orders order : orderList) {
+            List<OrderProduct> orderProducts = orderProductRepository.findTop6ByOrdersIdOrderByIdDesc(order.getId());
+            for (OrderProduct orderProduct : orderProducts) {
+                 responseOrderProductDto.add(ResponseOrderProductDto.shoppingList(
+                         orderProduct.getProduct().getId(),
+                         orderProduct.getProduct().getName(),
+                         orderProduct.getProduct().getThumbnailImg()
+                 ));
+            }
+        }
+        return responseOrderProductDto;
+    }
+
+}
