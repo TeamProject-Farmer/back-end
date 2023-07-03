d9928acb9e8f18afa0340cb23e4526f9b04b762b
jnk <dipper25@naver.com>
2023-07-04 06:24:46 +0900

    feat: 마이페이지 구매목록 > 주문내역 조회 기능 개발

--- src/main/java/com/farmer/backend/api/controller/orderproduct/OrderProductController.java
+++ src/main/java/com/farmer/backend/api/controller/orderproduct/OrderProductController.java
@@ -1,5 +1,7 @@
 package com.farmer.backend.api.controller.orderproduct;
 
+import com.farmer.backend.api.controller.orderproduct.request.RequestOrderProductStatusSearchDto;
+import com.farmer.backend.api.controller.orderproduct.response.ResponseOrderProductDetailDto;
 import com.farmer.backend.api.controller.orderproduct.response.ResponseOrderProductDto;
 import com.farmer.backend.api.service.orderproduct.OrderProductService;
 import com.farmer.backend.config.ApiDocumentResponse;
@@ -26,14 +28,29 @@ public class OrderProductController {
 
     /**
      * 마이페이지 구매목록 기능
-     * @param authentication
-     * @return
+     * @param authentication 세션 회원정보
+     * @return List<ResponseOrderProductDto>
      */
     @ApiDocumentResponse
-    @Operation(summary = "구매목록 조회", description = "현재 로그인 한 사용자의 구매 목록 리스트를 출력합니다.")
+    @Operation(summary = "마이페이지 구매목록 조회", description = "현재 로그인 한 사용자의 구매 목록 리스트를 출력합니다.")
     @GetMapping
     public List<ResponseOrderProductDto> shoppingList(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return orderProductService.shoppingList(userDetails.getUsername());
+    }
+
+    /**
+     * 마이페이지 구매목록 > 주문내역 조회
+     * @param statusSearchDto 날짜, 검색조건 정보
+     * @param authentication 세션 회원 정보
+     * @return List<ResponseOrderProductDetailDto>
+     */
+    @ApiDocumentResponse
+    @Operation(summary = "주문내역 전체 조회", description = "현재 로그인 한 사용자의 전체 주문내역을 조회합니다.")
+    @GetMapping("/order-list")
+    public List<ResponseOrderProductDetailDto> orderList(RequestOrderProductStatusSearchDto statusSearchDto,
+                                                         Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return orderProductService.orderList(statusSearchDto, userDetails.getUsername());
     }
 }
---
+++ src/main/java/com/farmer/backend/api/controller/orderproduct/request/RequestOrderProductStatusSearchDto.java
@@ -0,0 +1,23 @@
+package com.farmer.backend.api.controller.orderproduct.request;
+
+import com.farmer.backend.domain.orders.OrderStatus;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RequestOrderProductStatusSearchDto {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    private LocalDateTime startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    private LocalDateTime endDate;
+
+    private OrderStatus orderStatus;
+
+}
