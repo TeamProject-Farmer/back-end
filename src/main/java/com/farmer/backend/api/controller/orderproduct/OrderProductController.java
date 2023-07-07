22e72c7336440713517c32b4d33210a7c81f038f
jnk <dipper25@naver.com>
2023-07-07 09:40:41 +0900

    fix: OrderProductController 로그인 한 사용자 정보 파라미터 변경

--- src/main/java/com/farmer/backend/api/controller/orderproduct/OrderProductController.java
+++ src/main/java/com/farmer/backend/api/controller/orderproduct/OrderProductController.java
@@ -5,11 +5,14 @@ import com.farmer.backend.api.controller.orderproduct.response.ResponseOrderProd
 import com.farmer.backend.api.controller.orderproduct.response.ResponseOrderProductDto;
 import com.farmer.backend.api.service.orderproduct.OrderProductService;
 import com.farmer.backend.config.ApiDocumentResponse;
+import com.farmer.backend.domain.member.Member;
+import com.farmer.backend.login.general.MemberAdapter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,29 +31,28 @@ public class OrderProductController {
 
     /**
      * 마이페이지 구매목록 기능
-     * @param authentication 세션 회원정보
+     * @param member 세션 회원정보
      * @return List<ResponseOrderProductDto>
      */
+
     @ApiDocumentResponse
     @Operation(summary = "마이페이지 구매목록 조회", description = "현재 로그인 한 사용자의 구매 목록 리스트를 출력합니다.")
     @GetMapping
-    public List<ResponseOrderProductDto> shoppingList(Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        return orderProductService.shoppingList(userDetails.getUsername());
+    public List<ResponseOrderProductDto> shoppingList(@AuthenticationPrincipal MemberAdapter member) {
+        return orderProductService.shoppingList(member.getUsername());
     }
 
     /**
      * 마이페이지 구매목록 > 주문내역 조회
      * @param statusSearchDto 날짜, 검색조건 정보
-     * @param authentication 세션 회원 정보
+     * @param member 세션 회원 정보
      * @return List<ResponseOrderProductDetailDto>
      */
     @ApiDocumentResponse
     @Operation(summary = "주문내역 전체 조회", description = "현재 로그인 한 사용자의 전체 주문내역을 조회합니다.")
     @GetMapping("/order-list")
     public List<ResponseOrderProductDetailDto> orderList(RequestOrderProductStatusSearchDto statusSearchDto,
-                                                         Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        return orderProductService.orderList(statusSearchDto, userDetails.getUsername());
+                                                         @AuthenticationPrincipal MemberAdapter member) {
+        return orderProductService.orderList(statusSearchDto, member.getUsername());
     }
 }
