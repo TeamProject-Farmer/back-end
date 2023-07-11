774007e3c4f2967fb3bf05c113492da3327e0261
wolveshowl <dipper25@naver.com>
2023-07-11 20:40:33 +0900

    feat: 주문정보 입력 Dto 생성

---
+++ src/main/java/com/farmer/backend/api/controller/order/response/ResponseOrderInfoDto.java
@@ -0,0 +1,12 @@
+package com.farmer.backend.api.controller.order.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ResponseOrderInfoDto {
+
+}
