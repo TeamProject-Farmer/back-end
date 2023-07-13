1f61b67cdc9e44a0e6049db564ecdfbf3dd33691
jnk <dipper25@naver.com>
2023-07-14 01:15:12 +0900

    test: 회원 적립금 기능 테스트 코드 작성

--- src/test/java/com/farmer/backend/api/service/member/MemberServiceTest.java
+++ src/test/java/com/farmer/backend/api/service/member/MemberServiceTest.java
@@ -9,12 +9,14 @@ import com.farmer.backend.domain.memberscoupon.MemberCouponRepository;
 import com.farmer.backend.exception.CustomException;
 import com.farmer.backend.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
 @SpringBootTest
@@ -44,5 +46,13 @@ class MemberServiceTest {
         log.info(responseLoginMemberDto.getNickname());
 
 
+    }
+
+    @Test
+    @DisplayName("회원 적립금 조회")
+    void getPoint() {
+        Member member = memberRepository.findByEmail("kce2360@naver.com").orElseThrow(()->new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        Long point = memberRepository.findByEmail(member.getEmail()).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)).getPoint();
+        assertThat(point).isZero();
     }
 }
