9ee364c66f9d09a4b75fc23f902ec7aa755cf55a
chaening <kce2360@gmail.com>
2023-07-20 19:45:46 +0900

    fix : 상품 검색 파라미터값 변경

--- src/main/java/com/farmer/backend/api/service/search/SearchService.java
+++ src/main/java/com/farmer/backend/api/service/search/SearchService.java
@@ -2,7 +2,6 @@ package com.farmer.backend.api.service.search;
 
 import com.farmer.backend.api.controller.search.request.RequestSearchDto;
 import com.farmer.backend.api.controller.search.response.ResponseSearchPage;
-import com.farmer.backend.api.controller.search.request.SearchProductCondition;
 import com.farmer.backend.api.controller.search.response.ResponseSearchProductDto;
 import com.farmer.backend.domain.member.Member;
 import com.farmer.backend.domain.member.MemberRepository;
@@ -31,31 +30,31 @@ public class SearchService {
      * 상품 검색
      * @param pageable 페이징
      * @param sortSearchCond 정렬값 (recent - 신상품순, review - 리뷰 많은 순 , low - 낮은 가격 순, high - 높은 가격 순)
-     * @param searchProductCondition 검색어
+     * @param searchWord 검색어
      * @return 검색 상품
      */
     @Transactional
     public ResponseSearchPage searchProduct(String memberEmail,
                                             Pageable pageable,
                                             String sortSearchCond,
-                                            SearchProductCondition searchProductCondition) {
+                                            String searchWord) {
 
         List<String> memberSearchWord = null;
 
 
         Page<ResponseSearchProductDto> searchProduct =
-                searchQueryRepositoryImpl.searchProduct(pageable,sortSearchCond, searchProductCondition.getSearchWord().trim());
+                searchQueryRepositoryImpl.searchProduct(pageable,sortSearchCond, searchWord.trim());
 
 
 
         if(memberEmail!=null ){
             memberSearchWord = searchQueryRepositoryImpl.memberSearchWordList(memberEmail);
 
-            if(!memberSearchWord.contains(searchProductCondition.getSearchWord().trim())&& searchProduct.getTotalElements()!=0 ){
+            if(!memberSearchWord.contains(searchWord.trim())&& searchProduct.getTotalElements()!=0 ){
                 Member member = memberRepository.findByEmail(memberEmail).orElseThrow(()->new CustomException(ErrorCode.MEMBER_NOT_FOUND));
 
-                RequestSearchDto searchWord = new RequestSearchDto(member,searchProductCondition.getSearchWord().trim());
-                searchRepository.save(searchWord.toEntity(searchWord));
+                RequestSearchDto memberSearchWordList = new RequestSearchDto(member,searchWord.trim());
+                searchRepository.save(memberSearchWordList.toEntity(memberSearchWordList));
             }
 
 
