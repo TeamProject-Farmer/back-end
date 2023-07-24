d71cee167bd5ca513076913aea561fde31823973
chaening <kce2360@gmail.com>
2023-07-24 09:14:21 +0900

    feat : qna 작성

--- src/main/java/com/farmer/backend/api/controller/qna/QnAController.java
+++ src/main/java/com/farmer/backend/api/controller/qna/QnAController.java
@@ -1,15 +1,21 @@
 package com.farmer.backend.api.controller.qna;
 
+import com.farmer.backend.api.controller.qna.request.RequestQnAWriteDto;
 import com.farmer.backend.api.controller.qna.response.ResponseProductQnADto;
 import com.farmer.backend.api.service.qna.ProductQnAService;
 import com.farmer.backend.config.ApiDocumentResponse;
+import com.farmer.backend.domain.member.Member;
+import com.farmer.backend.login.general.MemberAdapter;
 import com.farmer.backend.paging.PageRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class QnAController {
 
     private final ProductQnAService productQnAService;
+
+    /**
+     * 문의사항 리스트 출력`
+     */
     @ApiDocumentResponse
     @Operation(summary = "QnA 리스트 전체 출력",description = "상품 QnA 리스트를 전체 출력합니다.")
     @GetMapping("/main/qna")
@@ -29,4 +39,23 @@ public class QnAController {
         pageRequest.setSize(5);
         return productQnAService.productQnA(pageRequest.of());
     }
+
+    /**
+     * 문의사항 작성
+     */
+    @ApiDocumentResponse
+    @Operation(summary = "QnA 작성", description = "상품 QnA 작성")
+    @PostMapping("/member/qna/write")
+    public ResponseEntity<String> qnaWrite(@AuthenticationPrincipal MemberAdapter memberAdapter, RequestQnAWriteDto requestQnAWriteDto){
+        Member member = memberAdapter.getMember();
+        return productQnAService.qnaWrite(member,requestQnAWriteDto);
+    }
+
+    /**
+     * 문의사항 상세보기
+     */
+
+    /**
+     * 내 문의사항만 보기
+     */
 }
--- src/main/java/com/farmer/backend/api/service/qna/ProductQnAService.java
+++ src/main/java/com/farmer/backend/api/service/qna/ProductQnAService.java
@@ -1,18 +1,46 @@
 package com.farmer.backend.api.service.qna;
 
+import com.farmer.backend.api.controller.qna.request.RequestQnAWriteDto;
 import com.farmer.backend.api.controller.qna.response.ResponseProductQnADto;
+import com.farmer.backend.api.controller.qna.response.ResponseQnAWriteDto;
+import com.farmer.backend.domain.admin.board.QnARepository;
+import com.farmer.backend.domain.admin.qna.Qna;
+import com.farmer.backend.domain.member.Member;
+import com.farmer.backend.domain.member.MemberRepository;
+import com.farmer.backend.domain.product.Product;
+import com.farmer.backend.domain.product.ProductRepository;
 import com.farmer.backend.domain.qna.ProductQnAQueryRepositoryImpl;
+import com.farmer.backend.exception.CustomException;
+import com.farmer.backend.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductQnAService {
     private final ProductQnAQueryRepositoryImpl productQnAQueryRepositoryImpl;
+    private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
+    private final QnARepository qnARepository;
+
     public Page<ResponseProductQnADto> productQnA(Pageable pageable) {
         return productQnAQueryRepositoryImpl.productQnAList(pageable);
+    }
+
+    @Transactional
+    public ResponseEntity<String> qnaWrite(Member member,RequestQnAWriteDto requestQnAWriteDto) {
+
+        Product product = productRepository.findById(requestQnAWriteDto.getProductId()).orElseThrow(()-> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+
+        Qna qnaWrite = requestQnAWriteDto.toEntity(product,member);
+        qnARepository.save(qnaWrite);
+
+        return new ResponseEntity<>("QNA 저장 완료", HttpStatus.OK);
+
     }
 }
