e3be3b9334d40abfb8786c6a13d094ced9c01619
jnk <dipper25@naver.com>
2023-08-09 06:12:50 +0900

    feat: 배송 도메인 리포지터리 추가

---
+++ src/main/java/com/farmer/backend/domain/delivery/DeliveryRepository.java
@@ -0,0 +1,6 @@
+package com.farmer.backend.domain.delivery;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+}
