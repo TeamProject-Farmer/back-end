f6d3752ba11675007b694601f0080c0dfa2f030c
jnk <dipper25@naver.com>
2023-08-09 22:32:56 +0900

    feat: 전체 처리 주문상태 필드 추가

--- src/main/java/com/farmer/backend/domain/orders/OrderStatus.java
+++ src/main/java/com/farmer/backend/domain/orders/OrderStatus.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum OrderStatus {
     WAIT("결제대기"), FAIL("주문실패"), DONE("주문완료"), CANCEL("주문취소"), REFUND("환불"), EXCHANGE("교환"), RETURN("반품"),
-    ORDER("주문");
+    ALL("전체 주문처리 상태");
 
     private String name;
 
